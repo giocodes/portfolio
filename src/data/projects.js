579b101cd0edbc73a211cc56f02aec76a5174981
Sergio Gomez <sigronline@gmail.com>
2017-09-28 07:12:58 -0500

    Order matters!

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,19 +1,12 @@
 export default [
-  {
-    id: 2,
-    name: 'HelloCodeJs',
-    description: 'Awesome static code visualizer',
-    tags: 'Node, React',
-    url: 'https://github.com/giocodes/helloCodeJS/',
-    label: 'GitHub Source',
-  },
+  
   {
     id: 3,
     name: 'Framework Wars',
     description: 'GitHub Reputation comparison tool',
     tags: 'GitHub API, ChartJS',
     url: 'https://github.com/giocodes/FrameworkWars',
-    label: 'Live Demo',
+    label: 'Source & Demo',
   },
   {
     id: 4,
@@ -21,7 +14,7 @@ export default [
     description: 'Responsive Single Page',
     tags: 'Angular, UI/UX',
     url: 'https://github.com/giocodes/guest-calendar',
-    label: 'Live Demo',
+    label: 'Source & Demo',
   },
   {
     id: 5,
@@ -29,7 +22,7 @@ export default [
     description: '3D Beatbox',
     tags: 'PaperJS, HowlerJS',
     url: 'https://github.com/giocodes/vr-beats',
-    label: 'Live Demo',
+    label: 'Readme',
   },
   {
     id: 1,
@@ -37,6 +30,14 @@ export default [
     description: 'Javascript memorable events timeline',
     tags: 'React, Firebase',
     url: 'https://giocodes.github.io/memorable/',
-    label: 'Live Demo',
+    label: 'Source & Demo',
   },
+{
+  id: 2,
+  name: 'HelloCodeJs',
+  description: 'Awesome static code visualizer',
+  tags: 'Node, React',
+  url: 'https://github.com/giocodes/helloCodeJS/',
+  label: 'GitHub Source',
+},  
 ];

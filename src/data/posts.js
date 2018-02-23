e81e3c0db9e80be9fcb9da7e5263116b90bc0de5
Sergio Gomez <sigronline@gmail.com>
2018-02-23 00:23:04 -0600

    Add new talk link

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,18 +1,24 @@
 export default [
   {
     id: 1,
+    title: 'Moving Your App to React: Lessons From a Real-World Experience',
+    link: 'https://docs.google.com/presentation/d/1rPg8RKK2hSTEi6Tqpb9qa1aHyCgKI01m2qKwT0aUHXw/edit',
+    description: 'The experience on moving features from a Java application to React and the challenge of creating a library of components to bring consistency throughout the new UI.',
+  },
+  {
+    id: 2,
     title: 'Future Javascript',
     link: 'https://www.thatconference.com/Sessions/Session/11471',
     description: 'Javascript, Node, ECMAScript, ES6, JS, oh so many names and concepts to keep up with, but don\'t worry! I got you covered.',
   },
   {
-    id: 2,
+    id: 3,
     title: 'Docker & Node starter',
     link: 'https://github.com/giocodes/docker-node',
     description: 'Docker is a great tool to ensure a consistent dev environment when sharing a project. Here´s a project starter to get you up and running.',
   },
   {
-    id: 3,
+    id: 4,
     title: 'Linting like you mean it',
     link: 'https://github.com/giocodes/react-eslint-prettier',
     description: 'Sometimes is more than trailing commas or the semicolon, here´s a basic ESLint + Prettier starter to the help.',

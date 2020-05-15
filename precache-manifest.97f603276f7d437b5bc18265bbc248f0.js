a1dca679a8a611e4d33505297aa52ff7962d85e6
Sergio Gomez <cosmos@Sergios-MacBook-Pro.local>
2020-05-14 21:54:10 -0700

    Updates

---
+++ precache-manifest.97f603276f7d437b5bc18265bbc248f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ea8aa9b84c03a52b9b16c11ce67bd1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f074a3793dece8a5b1b1",
+    "url": "/portfolio/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "12f3ad2e0920cb137771",
+    "url": "/portfolio/static/css/main.d855ddbc.chunk.css"
+  },
+  {
+    "revision": "f074a3793dece8a5b1b1",
+    "url": "/portfolio/static/js/2.19d529d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.19d529d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f3ad2e0920cb137771",
+    "url": "/portfolio/static/js/main.388def1d.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "85a82715a43e3de616f967667c2a6c67",
+    "url": "/portfolio/static/media/background-light.85a82715.jpg"
+  }
+]);
